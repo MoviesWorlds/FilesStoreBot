159e7e66505515670ccaab3b60fc5d9fd51ac365
EverseenMovies <therideepak3773@gmail.com>
2024-05-21 03:12:48 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,14 +4,14 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID","15554181"))
-	API_HASH = os.environ.get("API_HASH","c5524df816d0b2d6a137ea6066c7096f")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN","6992512716:AAE6pNZkfKXjjoG8yI08JjXs2wIHW_2BeSg")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME","EsmFilestream_bot")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL","-1002004732514"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER","1319079143"))
-	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://filesesm:filesesm@cluster0.r6b2pn7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","-1002126482175")
+	API_ID = int(os.environ.get("API_ID",""))
+	API_HASH = os.environ.get("API_HASH","")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN","")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME",)
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL",))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER",))
+	DATABASE_URL = os.environ.get("DATABASE_")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
